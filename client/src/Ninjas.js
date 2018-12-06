e00754f7289be1c53018250d34d04a1d1630cc34
sadam <sanjeet.kanekar@sjinnovation.com>
2018-12-06 19:18:51 +0530

    added contional output

--- client/src/Ninjas.js
+++ client/src/Ninjas.js
@@ -2,13 +2,14 @@ import React from 'react';
 
 const Ninjas = ({ninjas}) => {
         const ninjaList = ninjas.map(ninja => {
-            return(
-            <div key={ninja.id} className="ninja">
-                <div >Name: {ninja.name}</div>
-                <div>Age: {ninja.age}</div>
-                <div >Belt: {ninja.belt}</div>
-            </div>
-            )
+            return ninja.age > 20 ? (
+               <div key={ninja.id} className="ninja">
+                    <div >Name: {ninja.name}</div>
+                    <div>Age: {ninja.age}</div>
+                    <div >Belt: {ninja.belt}</div>
+                </div>
+            ) : null ;
+            
         })
         return(
             <div className="ninja-list">
