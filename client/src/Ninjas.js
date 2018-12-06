dd6084d59a91f64c9166d361f5210100ce14eeda
sadam <sanjeet.kanekar@sjinnovation.com>
2018-12-06 19:10:24 +0530

    changed Container to UI Component

--- client/src/Ninjas.js
+++ client/src/Ninjas.js
@@ -1,8 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Ninjas extends Component{
-    render(){
-        const {ninjas} = this.props;
+const Ninjas = ({ninjas}) => {
         const ninjaList = ninjas.map(ninja => {
             return(
             <div key={ninja.id} className="ninja">
@@ -17,7 +15,7 @@ class Ninjas extends Component{
                 {ninjaList}
             </div>
         )
-    }
 }
+
 
 export default Ninjas;
