6794a4eb13e71464ac81b2b0d44188761724cb80
sadam <sanjeet.kanekar@sjinnovation.com>
2018-12-06 18:50:03 +0530

    listed array values

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,19 @@ import React, { Component } from 'react';
 import Ninjas from './Ninjas'
 
 class App extends Component {
+  state = {
+    ninjas : [
+      { name: 'Ryu' ,age: 30, belt: 'black', id: 1 },
+      { name: 'yoshi' ,age: 20, belt: 'green', id: 2 },
+      { name: 'Crystal' ,age: 25, belt: 'pink', id: 3 }
+    ]
+  }
   render() {
     return (
       <div className="App">
-        <h1>My Fisrt React App</h1>
+        <h1>My First React App</h1>
         <p>Welcome :)</p>
-        <Ninjas name="Ryu" age="25" belt="black"/>
-        <Ninjas name="yoshi" age="30" belt="black"/>
+        <Ninjas ninjas={this.state.ninjas}/>
       </div>
     );
   }
