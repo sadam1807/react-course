b3e627b3fa977514867eb1df63d9499c0c2d17f4
sadam <sanjeet.kanekar@sjinnovation.com>
2018-12-06 20:37:53 +0530

    implemented add and delete data form list

--- client/src/App.js
+++ client/src/App.js
@@ -17,12 +17,24 @@ class App extends Component {
         ninjas : ninjas
       })
   }
+
+  deleteNinja = (id) => {
+    let ninjas = this.state.ninjas.filter(ninja =>{
+        return ninja.id !== id
+    })
+
+    this.setState({
+      ninjas : ninjas
+    })
+ }
+
+
   render() {
     return (
       <div className="App">
         <h1>My First React App</h1>
         <p>Welcome :)</p>
-        <Ninjas ninjas={this.state.ninjas}/>
+        <Ninjas deleteNinja={this.deleteNinja} ninjas={this.state.ninjas}/>
         <AddNinja addNinja={this.addNinja}/>
       </div>
     );
--- client/src/Ninjas.js
+++ client/src/Ninjas.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
-const Ninjas = ({ninjas}) => {
+const Ninjas = ({ninjas, deleteNinja}) => {
         const ninjaList = ninjas.map(ninja => {
             return ninja.age > 20 ? (
                <div key={ninja.id} className="ninja">
                     <div >Name: {ninja.name}</div>
                     <div>Age: {ninja.age}</div>
                     <div >Belt: {ninja.belt}</div>
+                    <button onClick={() => {deleteNinja(ninja.id)}} >Delete</button>
                 </div>
             ) : null ;
             
