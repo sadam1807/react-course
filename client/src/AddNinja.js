6ee4ca865eb097cc39542286b78c634ac994777d
sadam <sanjeet.kanekar@sjinnovation.com>
2018-12-06 19:45:10 +0530

    added form with DOM events

---
+++ client/src/AddNinja.js
@@ -0,0 +1,39 @@
+import React, {Component} from 'react'
+
+
+class AddNinja extends Component {
+    state = {
+        name: null,
+        age: null,
+        belt: null
+    }
+
+    handleChange = (e) => {
+        this.setState({
+           [e.target.id] : e.target.value
+        })
+    }
+    
+    handelSubmit = (e) => {
+        e.preventDefault();
+        console.log(this.state);
+    }
+
+    render(){
+        return(
+            <div>
+                <form onSubmit={this.handelSubmit}>
+                    <label htmlFor="name">Name: </label>
+                    <input type="text" id="name" onChange={this.handleChange}/>
+                    <label htmlFor="age">Age: </label>
+                    <input type="text" id="age" onChange={this.handleChange}/>
+                    <label htmlFor="belt">Belt: </label>
+                    <input type="text" id="belt" onChange={this.handleChange}/>
+                    <button>Submit</button>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default AddNinja;
